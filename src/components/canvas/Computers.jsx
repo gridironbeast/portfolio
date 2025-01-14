8c620e8f815d99830da6f85b4da4f1fc9c91d1e3
Nikolaychenko D <superpredator911@gmail.com>
2025-01-14 16:51:23 +0100

    made some adgusments

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,30 +3,43 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./asus_rog_strix_scar_17_2023_g733_gaming_laptop/scene.gltf")
+  const transformer = useGLTF("./transformers_-_animated_soundwave_character_rig/scene.gltf");
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" /> 
+      {/* Общее освещение */}
+      <hemisphereLight intensity={0.35} groundColor="black" />
+      
+      {/* Основной источник света сверху */}
       <spotLight
-        position={[0, 0, 0]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
+        position={[-20, 50, 10]}
+        angle={0.3}
+        penumbra={0.5}
+        intensity={1.5}
         castShadow
-        shadow-mapSize={1024} 
+        shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
-      <primitive 
-        object={computer.scene} 
-        scale={isMobile ? 1.3 : 1.5}
-        position={isMobile ? [0, -5.5, -2.2] : [0, -6.5, -1.5]}
-        rotation={[0, 0, 0]}
+      
+      {/* Точечный свет рядом с моделью */}
+      <pointLight position={[10, 10, 10]} intensity={2} />
+      
+      {/* Направленный свет для акцентов */}
+      <directionalLight
+        position={[5, 10, 5]}
+        intensity={0.5}
+        castShadow
+      />
+      
+      {/* Модель */}
+      <primitive
+        object={transformer.scene}
+        scale={isMobile ? 0.4 : 0.5} // Уменьшение масштаба модели
+        position={isMobile ? [0, -2.5, -2.2] : [0, -3, -1.5]} // Регулировка высоты модели
+        rotation={[-0.01, -0.2, -0.1]} // Небольшой наклон
       />
     </mesh>
-  )
-}
+  );
+};
 
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -47,21 +60,24 @@ const ComputersCanvas = () => {
     <Canvas
       frameloop="demand"
       shadows
-      camera={{ position: [18, 3, 5], fov: 25 }}
+      camera={{ position: [20, 5, 10], fov: 30 }} // Камера подальше от модели
       gl={{ preserveDrawingBuffer: true }}
       dpr={[1, 2]}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
+          autoRotate={true}
           maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2} 
+          minPolarAngle={Math.PI / 2}
+          // maxAzimuthAngle={Math.PI / 4}
+          // minAzimuthAngle={-Math.PI / 4}
         />
         <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
-  )
-}
+  );
+};
 
-export default ComputersCanvas; 
+export default ComputersCanvas;
